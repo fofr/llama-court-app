42c2b311ef7a6ee0b2ca50ec5433bc8cdb2eee6c
Paul Hayes <fofr@users.noreply.github.com>
2023-08-17 12:41:13 +0100

    Add proof of concept call

--- src/app/api/[id]/route.js
+++ src/app/api/[id]/route.js
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 import db from "../../../lib/db";
 
-export async function GET(request, context) {
+export async function GET(_request, context) {
   const params = context.params
   console.log(params)
-  return NextResponse.json({ error: 'Not found' }, { status: 404 })
+
+  let { data, error } = await db
+    .from('agents_state')
+    .select('*')
+    .eq('case_id', params.id)
+
+  return NextResponse.json({ data }, { status: 200 })
 }
