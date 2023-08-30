a1fb2e3ca54e81e2daa2840e64549a140dbbd978
Paul <fofr@users.noreply.github.com>
2023-08-30 13:45:08 +0100

    Add aria-kit

---
+++ src/components/Dialog.js
@@ -0,0 +1,24 @@
+import * as Ariakit from "@ariakit/react";
+
+export default function Example() {
+  const dialog = Ariakit.useDialogStore();
+  return (
+    <>
+      <Ariakit.Button onClick={dialog.show} className="button">
+        Show modal
+      </Ariakit.Button>
+      <Ariakit.Dialog store={dialog} className="dialog">
+        <Ariakit.DialogHeading className="heading">
+          Success
+        </Ariakit.DialogHeading>
+        <p className="description">
+          Your payment has been successfully processed. We have emailed your
+          receipt.
+        </p>
+        <div>
+          <Ariakit.DialogDismiss className="button">OK</Ariakit.DialogDismiss>
+        </div>
+      </Ariakit.Dialog>
+    </>
+  );
+}
