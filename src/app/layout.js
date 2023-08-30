2db9ec7488c46ea436508a799691825c93d6ad8e
Paul <fofr@users.noreply.github.com>
2023-08-30 14:55:53 +0100

    Add OG image

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="https://llama-court.replicate.dev/og.webp" />
+        <meta property="og:url" content="https://llama-court.replicate.dev" />
+        <meta property="og:type" content="website" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://llama-court.replicate.dev" />
+        <meta property="twitter:title" content={metadata.title} />
+        <meta property="twitter:description" content={metadata.description} />
+        <meta property="twitter:image" content="https://llama-court.replicate.dev/og.webp" />
+      </head>
       <body className={inter.className}>{children}</body>
       <Analytics />
     </html>
